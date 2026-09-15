b73538e757dec00c8d1073390d5efd7dbe274252
Saira Sharif <sairasharif5132@gmail.com>
2026-09-15 19:32:13 +0500

    Add Initial File

--- login.js
+++ login.js
@@ -1,17 +1,17 @@
-let loginFrom=document.getElementById("LoginFrom");
+let loginForm=document.getElementById("LoginForm");
 loginForm.addEventListener("submit", function(event) {
    event.preventDefault();
     let email =document.getElementById("email");
     let password =document.getElementById("password");
     let errorMessage =document.getElementById("errorMessage");
 if(email===""|| password===""){
-    erroemessage.innerhtml="Please enter email and password";
+    errorMessage.innerHTML="Please enter email and password";
 }else if(!email.includes("@")){
-     erroemessage.innerhtml="Please enter a valid email";
+     errorMessage.innerHTML="Please enter a valid email";
 }else if (password.length < 6) {
-     errorMessage.innerhtml="Password must be at least 6 characters";
+     errorMessage.innerHTML="Password must be at least 6 characters";
 } 
 else {
-     errorMessage.innerhtml="Login Successful!";
+     errorMessage.innerHTML="Login Successful!";
 }
 });
