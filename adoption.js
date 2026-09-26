2e60e898de4f1f4bda08872abf7942fa740bc968
Saira Sharif <sairasharif5132@gmail.com>
2026-09-26 10:01:31 +0500

    Updated code

--- adoption.js
+++ adoption.js
@@ -1,15 +1,21 @@
+let urlParams = new URLSearchParams(window.location.search);
+let selectedPet = urlParams.get("pet");
+if (selectedPet) {
+   document.getElementById("pet").value = selectedPet;
+
+}
 let adoptionForm=document.getElementById("adoptionForm");
 adoptionForm .addEventListener("submit", function(event) {
    event.preventDefault();
-    let name =document.getElementById("name");
-    let email =document.getElementById("email");
-    let phone =document.getElementById("phone");
-    let address =document.getElementById("address");
-    let pet =document.getElementById("pet");
-    let reason =document.getElementById("reason");
+    let name =document.getElementById("Name").value;
+    let email =document.getElementById("Email").value;
+    let phone =document.getElementById("Phone").value;
+    let address =document.getElementById("Address").value;
+    let pet =document.getElementById("Pet").value;
+    let reason =document.getElementById("Reason").value;
 
-    let errorMessage =document.getElementById("errorMessage");
-    let confirmation =document.getElementById("confirmation");
+    let errorMessage =document.getElementById("ErrorMessage");
+    let confirmation =document.getElementById("Confirmation");
 
      if (
         name === "" || 
@@ -24,12 +30,12 @@ adoptionForm .addEventListener("submit", function(event) {
     }else if (!email.includes("@")) {
         errorMessage.innerHTML = "Please enter a valid email.";
         confirmation.innerHTML = "";
-    }else if (phone.length < 11) {
+    }else if (phone.length !== 11) {
       errorMessage.innerHTML = "Please enter a valid phone number.";
       confirmation.innerHTML = "";
     }else {
       errorMessage.innerHTML = "";
-      confirmation.innerHTML = "";
+      confirmation.innerHTML =
             "<h2>Adoption Application Submitted Successfully! 🐾</h2>" +
             "<p>Thank you, " + name + ".</p>" +
             "<p>You have applied to adopt <b>" + pet + "</b>.</p>" +
